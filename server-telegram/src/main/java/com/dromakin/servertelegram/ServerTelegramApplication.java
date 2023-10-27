d4afcee7ebd5f5200b83413be1b2249603bbbabb
Dmitriy Romakin <32188375+dromakin@users.noreply.github.com>
2023-10-27 20:33:46 +0300

    init module server-telegram

---
+++ server-telegram/src/main/java/com/dromakin/servertelegram/ServerTelegramApplication.java
@@ -0,0 +1,13 @@
+package com.dromakin.servertelegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerTelegramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerTelegramApplication.class, args);
+    }
+
+}
---
+++ server-telegram/src/test/java/com/dromakin/servertelegram/ServerTelegramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dromakin.servertelegram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerTelegramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
